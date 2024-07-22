f80497a332ad1131ebf20ee785ac8408e76ffd2b
Fue Idriss Shulu <61651516+Jitomorin@users.noreply.github.com>
2024-07-23 00:34:19 +0300

    Fixed google login

--- components/GoogleOAuthProvider.tsx
+++ components/GoogleOAuthProvider.tsx
@@ -1,18 +1,14 @@
-"use client"
-import React from 'react'
-import { GoogleOAuthProvider } from '@react-oauth/google'
-const GoogleOAuthProvide = (
-    {children}: {children: React.ReactNode}
-) => {
+"use client";
+import React from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+const GoogleOAuthProvide = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-    <GoogleOAuthProvider clientId={
-        process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ""
-    }>
+      <GoogleOAuthProvider clientId="67588996400-ea4p83haedt9h4is518a97lc17k9lflk.apps.googleusercontent.com">
         {children}
-    </GoogleOAuthProvider> 
+      </GoogleOAuthProvider>
     </>
-  )
-}
+  );
+};
 
-export default GoogleOAuthProvide
+export default GoogleOAuthProvide;
